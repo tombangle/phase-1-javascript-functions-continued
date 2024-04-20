45a52eba0d925d7eab1743d8cd3b1462726ceb5e
Tom Bangle <tombangle@gmail.com>
2024-04-20 10:14:20 -0700

    complet lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`)
+}
+saturdayFun();
+saturdayFun(`bathe the dog`);
+
+const mondayWork = function (activity = `go to the office`) {
+    return (`This Monday, I will ${activity}.`)
+}
+mondayWork();
+mondayWork(`work from home`);
+
+
+function wrapAdjective(symbol){
+    function inner(adjective) {
+        return `You are ${symbol}${adjective}${symbol}!`;
+    } return inner;
+}
+const result_star = wrapAdjective(`*`);
+const result_pipe = wrapAdjective(`||`);
+
+const emphatic_star = result_star("a hard worker");
+const emphatic_pipe = result_pipe("a dedicated programmer");
+
+console.log(emphatic_star);
+console.log(emphatic_pipe);
